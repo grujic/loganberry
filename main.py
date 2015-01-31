53f7e75ea9f98d141db106676efad194ebc4ffc3
Tom Grujic <tomgrujic@Toms-MacBook-Pro.local>
2015-01-31 11:48:38 +0000

    Added a 'main.py' file

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+exchange_host = sys.argv[1]
+port_index = sys.argv[2]
+
+print("\n\n")
+print("exchange host address = " + exchange_host + "\n\n")
+print("port index = " + port_index + "\n\n")
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
