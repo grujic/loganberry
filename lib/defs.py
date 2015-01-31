5a519c154c26b5c7895ec4ff930ed2817d87b95d
Tom Grujic <tomgrujic@Toms-MacBook-Pro.local>
2015-01-31 12:16:06 +0000

    Added an object to interact with the exchange

---
+++ lib/defs.py
@@ -0,0 +1,22 @@
+
+class ExchangeConnecton:
+	# Main class for interacting with the exchange
+
+	def __init__(self):
+		pass
+
+	def addOrder(self):
+		# Add an order
+		pass
+
+	def convertOrder(self):
+		# Convert an ETH to its components
+		pass
+
+	def cancelOrder(self):
+		# Cancel an order
+		pass
+
+	def sayHello(self):
+		# Check connection
+		pass
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
 
+PUBLIC_EXCHANGE_PUBLIC_IP = "54.154.191.94"
+PUBLIC_EXCHANGE_PUBLIC_IP = "10.0.129.254"
+
 exchange_host = sys.argv[1]
 port_index = sys.argv[2]
 
@@ -7,5 +10,3 @@ print("\n\n")
 print("exchange host address = " + exchange_host + "\n\n")
 print("port index = " + port_index + "\n\n")
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
