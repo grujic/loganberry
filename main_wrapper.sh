032f0773381d160e105634911c38c7c688ee1620
Samuel Denny <samueljdenny@gmail.com>
2015-01-31 18:38:24 +0000

    dsdf

--- main_wrapper.sh
+++ main_wrapper.sh
@@ -2,17 +2,20 @@
 
 # Script to wrap main.py, stopping the market first, restarting it, running main, then stopping the market again.
 IDX=0
-TIME_BEFORE_STOP=10
+TIME_UNTIL_START=10  # in seconds
+TIME_WHILE_OPEN=10  # in seconds
 
 echo 'Stopping and resetting markets...'
 eth0 close-market 10.0.129.254:${IDX+47000}
 eth0 reset-positions 10.0.129.254:${IDX+47000} LOGANBERRY
 sleep 2
 
-echo 'Opening the market...'
-eth0 open-market 10.0.129.254:${IDX+47000}
 echo 'Running main.py...'
 python main.py 10.0.129.254 $IDX &
 
-sleep $TIME_BEFORE_STOP
+sleep $TIME_UNTIL_START
+echo 'Opening the market...'
+eth0 open-market 10.0.129.254:${IDX+47000}
+
+sleep $TIME_WHILE_OPEN
 eth0 close-market 10.0.129.254:${IDX+47000}
