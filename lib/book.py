0d4c9d9617fa658253ccd662249841002b34a12a
sperfiliev <sergei.perfiliev@gmail.com>
2015-01-31 18:35:14 +0000

    work in progress

--- lib/book.py
+++ lib/book.py
@@ -35,10 +35,10 @@ class Book:
         return [el[1] for el in self.data[ticker]['sell']]
 
     def get_best_buy_quote(self, ticker):
-        return sorted(self.get_ticker_buy_data(ticker), key = lambda x: x[0])[0]
+        return sorted(self.get_ticker_buy_data(ticker), key = lambda x: x[0])[-1]
 
     def get_best_sell_quote(self, ticker):
-        return sorted(self.get_ticker_sell_data(ticker), key = lambda x: x[0])[-1]
+        return sorted(self.get_ticker_sell_data(ticker), key = lambda x: x[0])[0]
 
     def nth_buy_price(self, ticker, n):
         pass
