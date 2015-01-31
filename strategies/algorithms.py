bc9b24501a450c615c4db0fb65bbf2f7c0718dab
sperfiliev <sergei.perfiliev@gmail.com>
2015-01-31 21:40:16 +0000

    work in progress

--- strategies/algorithms.py
+++ strategies/algorithms.py
@@ -50,13 +50,17 @@ def arbitrage_CORGE(conn):
 
 	for num_shares in xrange (multiplier, 100, -100):
 
-		corge_sell_price = multiplier * conn.book.get_vwap_buy_price("CORGE", multiplier)
-		foo_buy_price = multiplier * 0.3 * conn.book.get_vwap_sell_price("FOO", multiplier)
-		bar_buy_price = multiplier * 0.8 * conn.book.get_vwap_sell_price("BAR", multiplier)
+		corge_avg_price = conn.book.get_vwap_buy_price("CORGE", multiplier)
+		foo_avg_price = conn.book.get_vwap_sell_price("FOO", multiplier)
+		bar_avg_price = conn.book.get_vwap_sell_price("BAR", multiplier)
 
-		print "corge_sell_price: " + str(corge_sell_price)
-		print "foo_buy_price: " + str(foo_buy_price)
-		print "bar_buy_price: " + str(bar_buy_price)
+		print "corge_sell_price: " + str(corge_avg_price)
+		print "foo_buy_price: " + str(foo_avg_price)
+		print "bar_buy_price: " + str(bar_avg_price)
+
+		corge_sell_price = multiplier * corge_avg_price
+		foo_buy_price = multiplier * 0.3 * foo_avg_price
+		bar_buy_price = multiplier * 0.8 * bar_avg_price
 
 		if(corge_sell_price > foo_buy_price + bar_buy_price + 100):
 			#Buy Foo:
@@ -78,13 +82,17 @@ def arbitrage_CORGE(conn):
 			conn.convertOrder("CORGE", "BUY", multiplier)
 
 
-		corge_buy_price = multiplier * conn.book.get_vwap_sell_price("CORGE", multiplier)
-		foo_sell_price = multiplier * 0.3 * conn.book.get_vwap_buy_price("FOO", multiplier)
-		bar_sell_price = multiplier * 0.8 * conn.book.get_vwap_buy_price("BAR", multiplier)
+		corge_avg_price = conn.book.get_vwap_sell_price("CORGE", multiplier)
+		foo_avg_price = conn.book.get_vwap_buy_price("FOO", multiplier)
+		bar_avg_price = conn.book.get_vwap_buy_price("BAR", multiplier)
 
 		print "corge_buy_price: " + str(corge_buy_price)
 		print "foo_sell_price: " + str(foo_sell_price)
 		print "bar_sell_price: " + str(bar_sell_price)
+
+		corge_buy_price = multiplier * corge_avg_price
+		foo_sell_price = multiplier * 0.3 * foo_avg_price
+		bar_sell_price = multiplier * 0.8 * bar_avg_price
 
 		if(corge_buy_price < foo_sell_price + bar_sell_price - 100):
 			#Sell Foo:
