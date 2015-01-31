378bac5e28692e34864006f41769ed5cd2511e08
Tom Grujic <tomgrujic@Toms-MacBook-Pro.local>
2015-01-31 20:14:38 +0000

    trying to get rid of fucking tabs

--- strategies/algorithms.py
+++ strategies/algorithms.py
@@ -15,31 +15,30 @@ def buy_everything_at_best_ask(conn):
         if best_sell_quote == -1:
             return
 
-		print "Best sell quote for " + ticker + " is " + str(best_sell_quote)
-		print conn.book.get_ticker_sell_data(ticker)
+        print "Best sell quote for " + ticker + " is " + str(best_sell_quote)
+        print conn.book.get_ticker_sell_data(ticker)
 
-		order_id = conn.addOrder(ticker, "BUY", 0.9 * best_sell_quote[0], size)
+        order_id = conn.addOrder(ticker, "BUY", 0.9 * best_sell_quote[0], size)
 
-		quote_record = Quote(ticker, "BUY", "Alpha", 0.9 * best_sell_quote[0], size, order_id)
-		conn.quotes.addQuote(quote_record)
+        quote_record = Quote(ticker, "BUY", "Alpha", 0.9 * best_sell_quote[0], size, order_id)
+        conn.quotes.addQuote(quote_record)
 
 
 def sell_everything_at_best_bid(conn):
-	print "Running sell_everything_at_best_bid() function"
+    print "Running sell_everything_at_best_bid() function"
+    tickers = [ "FOO", "BAR", "BAZ", "QUUX", "CORGE" ]
+    size = 100
 
-	tickers = [ "FOO", "BAR", "BAZ", "QUUX", "CORGE" ]
-	size = 100
+    for ticker in tickers:
+    	best_buy_quote = conn.book.get_best_buy_quote(ticker)
 
-	for ticker in tickers:
-		best_buy_quote = conn.book.get_best_buy_quote(ticker)
-
-        if best_buy_quote == -1:
+    	if best_buy_quote == -1:
             return
 
-		print "Best sell quote for " + ticker + " is " + str(best_buy_quote)
-		print conn.book.get_ticker_buy_data(ticker)
+        print "Best sell quote for " + ticker + " is " + str(best_buy_quote)
+        print conn.book.get_ticker_buy_data(ticker)
 
-		order_id = conn.addOrder(ticker, "SELL", 1.1 * best_buy_quote[0], size)
+        order_id = conn.addOrder(ticker, "SELL", 1.1 * best_buy_quote[0], size)
 
-		quote_record = Quote(ticker, "SELL", "Alpha", 1.1 * best_buy_quote[0], size, order_id)
-		conn.quotes.addQuote(quote_record)
+        quote_record = Quote(ticker, "SELL", "Alpha", 1.1 * best_buy_quote[0], size, order_id)
+        conn.quotes.addQuote(quote_record)
