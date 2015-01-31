10f3b94f66d457efc533f11c9393638c8ffd7c57
sperfiliev <sergei.perfiliev@gmail.com>
2015-01-31 20:56:36 +0000

    work in progress

--- lib/book.py
+++ lib/book.py
@@ -46,7 +46,7 @@ class Book:
         if len(sell_data) ==0 :
             return -1
         else:
-            return sorted(sell_data, key = lambda x: x[0])[0]
+            return sorted(sell_data, key = lambda x: x[0])[-1]
 
     def how_many_sells(self, ticker):
         return len(self.get_ticker_sell_data(ticker))
@@ -82,11 +82,9 @@ class Book:
         
         print sell_data
 
-        sorted(self.get_ticker_sell_data(ticker), key = lambda x: x[0])[0]
+        sorted(sell_data, key = lambda x: x[0])[-1]
 
         print sell_data
-
-        i = 0
 
         for quote in sell_data:
             price = quote[0]
@@ -99,4 +97,35 @@ class Book:
                 vwap += size * price / shares
                 need_to_buy -= size
 
-            i++
+        if(need_to_buy > 0):
+        	return 99999999
+        else:
+        	return vwap
+
+    def get_vwap_buy_price(self, ticker, shares):
+        vwap = 0
+        need_to_sell = shares
+
+        buy_data = self.get_ticker_buy_data(ticker)
+        
+        print buy_data
+
+        sorted(buy_data, key = lambda x: x[0])[0]
+
+        print buy_data
+
+        for quote in buy_data:
+            price = quote[0]
+            size = quote[1]
+
+            if(size >= need_to_sell):
+                vwap += need_to_sell * price
+                need_to_sell = 0
+            else:
+                vwap += size * price / shares
+                need_to_sell -= size
+
+        if(need_to_buy > 0):
+            return 0
+        else:
+            return vwap
