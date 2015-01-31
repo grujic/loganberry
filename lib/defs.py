a5a328468d0be61ef69ca35207db6f9e252164b3
Samuel Denny <samueljdenny@gmail.com>
2015-01-31 16:00:49 +0000

    Adding log

--- lib/defs.py
+++ lib/defs.py
@@ -4,7 +4,7 @@ import json
 from lib.book import Book
 
 import logging
-logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 #logger = logging.getLogger('loganberry')
 
 
